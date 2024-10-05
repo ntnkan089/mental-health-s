d0814631167b488fc320ea8f4faad441aa8a0011
ntnkan089@gmail.com <ntnkan089@gmail.com>
2024-10-04 22:01:01 -0700

    v0.

---
+++ csCompile/lab1/lab1/target/debug/deps/lex.d
@@ -0,0 +1,5 @@
+C:\Users\ntnka\proja\rus\lex\target\debug\deps\lex.exe: src/main.rs
+
+C:\Users\ntnka\proja\rus\lex\target\debug\deps\lex.d: src/main.rs
+
+src/main.rs:
---
+++ csCompile/lab1/lab1/target/debug/lex.d
@@ -0,0 +1,1 @@
+C:\Users\ntnka\proja\rus\lex\target\debug\lex.exe: C:\Users\ntnka\proja\rus\lex\src\main.rs

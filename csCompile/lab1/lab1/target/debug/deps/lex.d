f43e543a562677dd621fb511b16a05afc5d4d7be
ntnkan089 <102566544+ntnkan089@users.noreply.github.com>
2024-10-07 14:18:18 -0700

    Delete csCompile/lab1/lab1 directory

--- csCompile/lab1/lab1/target/debug/deps/lex.d
+++
@@ -1,5 +0,0 @@
-C:\Users\ntnka\proja\rus\lex\target\debug\deps\lex.exe: src/main.rs
-
-C:\Users\ntnka\proja\rus\lex\target\debug\deps\lex.d: src/main.rs
-
-src/main.rs:
--- csCompile/lab1/lab1/target/debug/lex.d
+++
@@ -1,1 +0,0 @@
-C:\Users\ntnka\proja\rus\lex\target\debug\lex.exe: C:\Users\ntnka\proja\rus\lex\src\main.rs
